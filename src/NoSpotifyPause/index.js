1d58e1815af65be37716101c38601aece9e757b8
Paul Oppenheimer <bepvte@gmail.com>
2021-04-02 11:55:49 -0400

    add NoSpotifyPause

---
+++ src/NoSpotifyPause/index.js
@@ -0,0 +1,12 @@
+module.exports = (Plugin, Library) => {
+  const { Patcher, WebpackModules } = Library;
+  return class NoSpotifyPause extends Plugin {
+    onStart() {
+      const spotify = WebpackModules.getByProps("fetchIsSpotifyProtocolRegistered");
+      Patcher.instead(spotify, "pause", function(){})
+    }
+    onStop() {
+      Patcher.unpatchAll();
+    }
+  };
+};
