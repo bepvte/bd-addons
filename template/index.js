1372fc1c18346d4dc1d41bfbc6abad68bff227c9
Paul Oppenheimer <bepvte@gmail.com>
2021-04-02 11:50:54 -0400

    add a template and prettierrc

---
+++ template/index.js
@@ -0,0 +1,9 @@
+module.exports = (Plugin, Library) => {
+  const { Patcher, WebpackModules } = Library;
+  return class FILL extends Plugin {
+    onStart() {
+    }
+    onStop() {
+    }
+  };
+};
