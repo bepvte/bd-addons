3817bb9f775df8d91b8816995d0d56bbfd2a66a5
bep <8226605+bepvte@users.noreply.github.com>
2021-04-02 14:29:37 -0400

    Update StereoSound

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ My plugins for [BetterDiscord](https://github.com/rauenzi/BetterDiscordApp)
 
 * [**GifSaver**](https://raw.githubusercontent.com/bepvte/bd-addons/main/plugins/gifsaver.plugin.js): Automatically backs up your favorited gifs in your plugins folder, and then restores them if discord erases them
 * [**CanaryLinks**](https://raw.githubusercontent.com/bepvte/bd-addons/main/plugins/CanaryLinks.plugin.js): When clicking "Copy Message Link" on Discord Canary or PTB, make links start with `https://discord.com/channels/` instead of `https://canary.discord.com/channels/`
-* [**StereoSound**](https://raw.githubusercontent.com/bepvte/bd-addons/main/plugins/StereoSound.plugin.js): Tries to add stereo to voice calls. Buggy and I can't test it very well.
+* [**StereoSound**](https://raw.githubusercontent.com/bepvte/bd-addons/main/plugins/StereoSound.plugin.js): Adds stereo sound to voice calls. Requires a capable stereo microphone.
--- src/StereoSound/index.js
+++ src/StereoSound/index.js
@@ -1,34 +1,56 @@
 module.exports = (Plugin, Library) => {
-  const { WebpackModules, Logger } = Library;
+  const { WebpackModules, Patcher, Toasts } = Library;
 
   return class StereoSound extends Plugin {
     onStart() {
-      this.voiceModule = WebpackModules.getByPrototypes("setSelfDeaf");
+      const voiceSettingsStore = WebpackModules.getByProps("getEchoCancellation");
+      if (
+        voiceSettingsStore.getNoiseSuppression() ||
+        voiceSettingsStore.getNoiseCancellation() ||
+        voiceSettingsStore.getEchoCancellation()
+      ) {
+        if (this.settings.enableToasts) {
+          Toasts.show(
+            "Please disable echo cancellation, noise reduction, and noise suppression for StereoSound",
+            { type: "warning", timeout: 5000 }
+          );
+        }
+        // This would not work, noise reduction would be stuck to on
+        // const voiceSettings = WebpackModules.getByProps("setNoiseSuppression");
+        // 2nd arg is for analytics
+        // voiceSettings.setNoiseSuppression(false, {});
+        // voiceSettings.setEchoCancellation(false, {});
+        // voiceSettings.setNoiseCancellation(false, {});
+      }
 
-      // not using patcher because we are just copying from PC plugin
-      // I dont want to think about `this` or anything
-      const initialize = this.voiceModule.prototype.initialize;
-      this.originalInitialize = initialize;
-      this.voiceModule.prototype.initialize = function () {
-        initialize.call(this, ...arguments);
-        const setTransportOptions = this.conn.setTransportOptions;
-        this.conn.setTransportOptions = function (obj) {
-          if (obj.audioEncoder) {
-            obj.audioEncoder.params = {
-              stereo: "2",
-            };
-            obj.audioEncoder.channels = 2;
-          }
-          if (obj.fec) {
-            obj.fec = false;
-          }
-          setTransportOptions.call(this, obj);
-          Logger.info("Initialization worked for this voice call");
-        };
+      const voiceModule = WebpackModules.getByPrototypes("setSelfDeaf");
+      Patcher.after(voiceModule.prototype, "initialize", this.replacement.bind(this));
+    }
+    replacement(thisObj, _args, ret) {
+      const setTransportOptions = thisObj.conn.setTransportOptions;
+      thisObj.conn.setTransportOptions = function (obj) {
+        if (obj.audioEncoder) {
+          obj.audioEncoder.params = {
+            stereo: "2",
+          };
+          obj.audioEncoder.channels = 2;
+        }
+        if (obj.fec) {
+          obj.fec = false;
+        }
+        setTransportOptions.call(thisObj, obj);
       };
+      if (this.settings.enableToasts) {
+        Toasts.info("Stereo calling enabled");
+      }
+      return ret;
     }
     onStop() {
-      this.voiceModule.prototype.initialize = this.originalInitialize;
+      Patcher.unpatchAll();
+    }
+    getSettingsPanel() {
+      const panel = this.buildSettingsPanel();
+      return panel.getElement();
     }
   };
 };
