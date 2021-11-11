99310bd5fdc7ac63d6cfaaaf19c3713fd14237a3
bep <8226605+bepvte@users.noreply.github.com>
2021-11-11 17:49:38 -0500

    Change gifsaver to be a backup exporter

--- plugins/gifsaver.plugin.js
+++ plugins/gifsaver.plugin.js
@@ -1,6 +1,6 @@
 /**
  * @name GifSaver
- * @version 1.0.0
+ * @version 1.0.1
  * @description Backups the list of favorited gifs inside your plugins folder.
  * @author bepvte
  * @authorLink https://github.com/bepvte
@@ -44,8 +44,8 @@ module.exports = (() => {
 				discord_id: "355434532893360138",
 				github_username: "TheGameratorT"
 			}],
-			version: "1.0.0",
-			description: "Automatically backs up your favorited GIFs in your plugins folder, and then restores them if Discord erases them.",
+			version: "2.0.0",
+			description: "Feel free to delete, remainder of gifsaver",
 			github: "https://github.com/bepvte/bd-addons",
 			github_raw: "https://raw.githubusercontent.com/bepvte/bd-addons/main/plugins/gifsaver.plugin.js"
 		},
@@ -63,18 +63,10 @@ module.exports = (() => {
 			value: true
 		}],
 		changelog: [{
-			title: "Rewritten!",
-			type: "improved",
-			items: ["GIFs are now automatically restored", "GIFs are now able to be saved per-account in settings"]
-		}, {
-			title: "Notes",
-			items: [
-				"We no longer check the `gifbackup.json` file, and now use the `GifSaver.config.json`",
-				"Feel free to delete `gifbackup.json`",
-				"We now require Zere's plugin library",
-				"If you see any bugs or are annoyed by any new behavior, feel free to leave a message" +
-					" on the https://github.com/bepvte/bd-addons/issues or ping me in the support server"
-			]
+			title: "Network GIF backup is now part of discord!!",
+			items: ["GIFSaver will no longer function due to these changes being part of discord",
+                    "There is unfortunately now a 250 gif limit, which might have hidden some of your gifs",
+                    "Because of this, I have exported your last Gif Backup to your plugins folder as `gifs_export.html`"]
 		}]
 	};
 
@@ -104,141 +96,59 @@ module.exports = (() => {
 
 	const plugin = (Plugin, Api) => {
 
-	const {
-		WebpackModules,
-		DiscordModules,
-		PluginUtilities,
-		Toasts
-	} = Api;
-
-	const {
-		UserStore,
-		Dispatcher,
-		DiscordConstants
-	} = DiscordModules;
-
-	const ActionTypes = DiscordConstants.ActionTypes;
-
 	return class GifSaver extends Plugin {
 
 	// When the plugin starts
 	onStart() {
-		this.objectStorage = WebpackModules.getByProps("ObjectStorage");
-		this.gifStore = WebpackModules.getByProps("getRandomFavorite");
+        const path = require("path");
+        const fs = require("fs");
+        const loc = path.resolve(BdApi.Plugins.folder, "gifsaver.config.json");
+        const data = JSON.parse(fs.readFileSync(loc));
+        let outbuf = `<!doctype html>
+<title>Gif export</title>
+<body>
+`;
+        for (const x in data) {
+            if (x === "default") {
+                outbuf += "<h3>The main users gifs:</h3>\n<ul>\n";
+                for (const y of data["default"]) {
+                    outbuf += `<li><a href="${y.url}">${y.url}</a>\n<br/>`;
+                    if (y.format === "VIDEO") {
+                        outbuf += `<video controls loop autoplay muted src="${y.src}"></video>`;
+                    } else {
+                        outbuf += `<img src="${y.src}"/>`;
+                    }
+                    outbuf += `</li>`;
+                }
+                outbuf += "</ul>\n";
+            }
+            if (!isNaN(x)) {
+                outbuf += `<h3>The gifs of user ${x}:</h3>\n<ul>\n`;
+                for (const y of data[x]) {
+                    outbuf += `<li><a href="${y.url}">${y.url}</a>\n<br/>`;
+                    if (y.format === "VIDEO") {
+                        outbuf += `<video controls loop autoplay muted src="${y.src}"></video>`;
+                    } else {
+                        outbuf += `<img src="${y.src}"/>`;
+                    }
+                    outbuf += `</li>`;
+                }
+                outbuf += "</ul>\n";
+            }
+        }
+        outbuf += "</body>";
+        const outpath = path.resolve(BdApi.Plugins.folder, "gifs_export.html");
+        fs.writeFileSync(outpath, outbuf);
 
-		Dispatcher.subscribe(ActionTypes.CONNECTION_OPEN, this.initialize); // Recover favorites after login
-		this.gifStore.addChangeListener(this.onGifStoreChange); // Backup favorites on GIF store interaction
+        BdApi.showConfirmationModal("GIF backup exported", "Your gif backup has been exported safely. Do you want to open it in your browser?\nTo disable this notice, disable or remove GifSaver.",
+                                    {confirmText: "Open", cancelText: "Cancel",
+                                     onConfirm: () => {
+										 const shell = require("electron").shell;
+										 const open = shell.openItem || shell.openPath;
+										 open(outpath);
+                                     }});
 
-		/**
-		 * Initialization needs to be here as well, because plugins
-		 * are only loaded after CONNECTION_OPEN.
-		 * The CONNECTION_OPEN subscription is only there
-		 * to allow favorites to be restored right after logging in.
-		 */
-		this.initialize();
-	}
-
-	// When the plugin stops
-	onStop() {
-		Dispatcher.unsubscribe(ActionTypes.CONNECTION_OPEN, this.initialize);
-		this.gifStore.removeChangeListener(this.onGifStoreChange);
-	}
-
-	// When interacting with the settings
-	getSettingsPanel() {
-		const panel = this.buildSettingsPanel();
-		panel.addListener((id, value) => {
-			if (id == "shareFavorites") {
-				// Switch between shared and per-user
-				const favorites = this.readBackup();
-				this.restoreData(favorites);
-				this.showToast(2, value);
-			}
-			if (id == "enableToasts") {
-				// Toast only shown when enabling
-				this.showToast(3, 0);
-			}
-		});
-		return panel.getElement();
-	}
-
-	// Function that runs on plugin start and login
-	initialize = () => {
-		/**
-		 * Read the backup and get the favorites.
-		 *
-		 * If there are no favorites, assume they
-		 * got lost and try to restore them if
-		 * the backup isn't empty.
-		 *
-		 * Otherwise, if the backup is empty
-		 * but there are favorites, assume that
-		 * it's the first use of the plugin and
-		 * save the favorites in the backup.
-		 */
-		const backup = this.readBackup();
-		const favorites = this.gifStore.getFavorites();
-
-		if (favorites.length == 0 && backup.length > 0) { // No favorites but backup
-			this.restoreData(backup);
-			this.showToast(0, backup.length);
-		}
-		else if (backup.length == 0 && favorites.length > 0) { // No backup but favorites
-			this.backupData(favorites);
-			this.showToast(1, favorites.length);
-		}
-	}
-
-	// Backup the favorites on store change
-	onGifStoreChange = () => {
-		const favorites = this.gifStore.getFavorites();
-		this.backupData(favorites);
-	}
-
-	// Writes the favorites to the backup
-	backupData(favorites) {
-		const key = this.getSaveKey();
-		PluginUtilities.saveData(this.getName(), key, favorites);
-	}
-
-	// Writes the favorites to the internal storage and re-initializes them
-	restoreData(favorites) {
-		const state = {
-			favorites: favorites,
-			timesFavorited: favorites.length
-		};
-		const store = {
-			_state: state,
-			_version: 2
-		};
-		this.objectStorage.impl.set("GIFFavoritesStore", store);
-		this.gifStore.initialize(state);
-	}
-
-	// Gets the save key and returns the backup
-	readBackup() {
-		const key = this.getSaveKey();
-		return PluginUtilities.loadData(this.getName(), key, []);
-	}
-
-	// Shows a toast if toasts are enabled
-	showToast(id, value) {
-		if (this.settings.enableToasts) {
-			var msg;
-			switch (id) {
-				case 0: { msg = `Restored ${value} favorite GIFs.`; break; }
-				case 1: { msg = `Backed up ${value} favorites GIFs.`; break; }
-				case 2: { msg = `Switched to ${value ? "shared" : "per-user"} favorite GIFs.`; break; }
-				case 3: { msg = "Enabled toasts."; break; }
-			}
-
-			setTimeout(() => Toasts.success(msg), 1);
-		}
-	}
-
-	// Returns the key to use when acessing the backup
-	getSaveKey() {
-		return this.settings.shareFavorites ? "default" : UserStore.getCurrentUser().id;
+		// Dispatcher.subscribe(ActionTypes.CONNECTION_OPEN, this.initialize); // Recover favorites after login
 	}
 
 	};
