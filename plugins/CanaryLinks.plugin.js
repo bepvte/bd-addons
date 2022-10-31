0f0433c2b8e7d9e05385565a79d1ee84fb9a4add
bepvte <8226605+bepvte@users.noreply.github.com>
2022-10-31 19:21:33 -0400

    back to normal

--- plugins/CanaryLinks.plugin.js
+++ plugins/CanaryLinks.plugin.js
@@ -1,110 +1,182 @@
 /**
- * @name CanaryLinks
- * @version 0.0.2
+ * @name Canary Links
+ * @description Makes "copy message link" not begin with canary.discord.com or ptb.discord.com
+ * @version 0.0.7
+ * @author bep
+ * @authorId 147077474222604288
  * @authorLink https://github.com/bepvte
+ * @website https://github.com/bepvte/bd-addons
  * @source https://raw.githubusercontent.com/bepvte/bd-addons/main/plugins/CanaryLinks.plugin.js
  */
 /*@cc_on
 @if (@_jscript)
-	
-	// Offer to self-install for clueless users that try to run this directly.
-	var shell = WScript.CreateObject("WScript.Shell");
-	var fs = new ActiveXObject("Scripting.FileSystemObject");
-	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
-	var pathSelf = WScript.ScriptFullName;
-	// Put the user at ease by addressing them in the first person
-	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-		shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-	} else if (!fs.FolderExists(pathPlugins)) {
-		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-		// Show the user where to put plugins in the future
-		shell.Exec("explorer " + pathPlugins);
-		shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
-	}
-	WScript.Quit();
+    
+    // Offer to self-install for clueless users that try to run this directly.
+    var shell = WScript.CreateObject("WScript.Shell");
+    var fs = new ActiveXObject("Scripting.FileSystemObject");
+    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
+    var pathSelf = WScript.ScriptFullName;
+    // Put the user at ease by addressing them in the first person
+    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+    } else if (!fs.FolderExists(pathPlugins)) {
+        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+        // Show the user where to put plugins in the future
+        shell.Exec("explorer " + pathPlugins);
+        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+    }
+    WScript.Quit();
 
 @else@*/
-
-module.exports = (() => {
-    const config = {"main":"index.js","info":{"name":"Canary Links","authors":[{"name":"bep","discord_id":"147077474222604288","github_username":"bepvte"}],"authorLink":"https://github.com/bepvte","version":"0.0.2","description":"Makes \"copy message link\" not begin with canary.discord.com on canary. Also works on PTB.","github":"https://github.com/bepvte/bd-addons","github_raw":"https://raw.githubusercontent.com/bepvte/bd-addons/main/plugins/CanaryLinks.plugin.js","changelog":[{"title":"Improvements","type":"improved","items":["Fixed plugin not working on the 3 dots menu copy link button, and shift click copy link button"]}]}};
-
-    return !global.ZeresPluginLibrary ? class {
-        constructor() {this._config = config;}
-        getName() {return config.info.name;}
-        getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
-        getDescription() {return config.info.description;}
-        getVersion() {return config.info.version;}
-        load() {
-            BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
-                confirmText: "Download Now",
-                cancelText: "Cancel",
-                onConfirm: () => {
-                    require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
-                        if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
-                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
+const config = {
+    main: "index.js",
+    name: "Canary Links",
+    author: "bep",
+    authorId: "147077474222604288",
+    authorLink: "https://github.com/bepvte",
+    version: "0.0.7",
+    description: "Makes \"copy message link\" not begin with canary.discord.com or ptb.discord.com",
+    github: "https://github.com/bepvte/bd-addons",
+    github_raw: "https://raw.githubusercontent.com/bepvte/bd-addons/main/plugins/CanaryLinks.plugin.js",
+    changelog: [
+        {
+            title: "Fixes",
+            type: "fixed",
+            items: [
+                "Fixes the plugin for revamped discord! You will need the latest 0PluginLibrary for it to work"
+            ]
+        }
+    ]
+};
+class Dummy {
+    constructor() {this._config = config;}
+    start() {}
+    stop() {}
+}
+ 
+if (!global.ZeresPluginLibrary) {
+    BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.name ?? config.info.name} is missing. Please click Download Now to install it.`, {
+        confirmText: "Download Now",
+        cancelText: "Cancel",
+        onConfirm: () => {
+            require("request").get("https://betterdiscord.app/gh-redirect?id=9", async (err, resp, body) => {
+                if (err) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+                if (resp.statusCode === 302) {
+                    require("request").get(resp.headers.location, async (error, response, content) => {
+                        if (error) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), content, r));
                     });
+                }
+                else {
+                    await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
                 }
             });
         }
-        start() {}
-        stop() {}
-    } : (([Plugin, Api]) => {
-        const plugin = (Plugin, Library) => {
-  const { Patcher, WebpackModules, Filters } = Library;
+    });
+}
+ 
+module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
+     const plugin = (Plugin, Library) => {
+  const { Patcher, Filters } = Library;
+  const Webpack = BdApi.Webpack;
   return class CanaryLinks extends Plugin {
     onStart() {
-      // the ClipboardUtils module has no displayname and is only recognizable
-      // through its singular `copy` function, but many modules have a `copy` property
-      this.ClipboardUtils = WebpackModules.getModule((obj) => {
-        const keys = Object.keys(obj);
-        // so we find a module with only `copy`
-        return keys.length === 1 && keys[0] === "copy";
-      }, true);
+      // discords copy to clipboard function
+      this.copy = Webpack.getModule(
+        Filters.combine(
+          (m) => m?.length === 1,
+          Filters.byString("ClipboardUtils.copy()")
+        ),
+        { searchExports: true }
+      );
 
-      // we have to use getByIndex to get the 'raw' module, because the module exports just a function
-      // this is the thing in the right click menu
-      const copyLinkItem = WebpackModules.getByIndex(
-        WebpackModules.getIndex(Filters.byDisplayName("useMessageCopyLinkItem"))
+      this.Domain = "discord.com";
+      this.Routes = Webpack.getModule(
+        Filters.byProperties(["CHANNEL", "MESSAGE_REQUESTS"]),
+        {
+          searchExports: true,
+        }
       );
-      Patcher.after(
-        copyLinkItem,
-        "default",
-        this.linkItemReplacement.bind(this)
-      );
+
+      // message link copy
+      Webpack.waitForModule(Filters.byString("COPY_MESSAGE_LINK"), {
+        defaultExport: false,
+      }).then((copyLinkItem) => {
+        Patcher.after(copyLinkItem, "Z", this.messageCopyLink.bind(this));
+      });
+
+      // channel link copy
+      Webpack.waitForModule(Filters.byString('id:"channel-copy-link"'), {
+        defaultExport: false,
+      }).then((copyLinkItem) => {
+        Patcher.after(copyLinkItem, "Z", this.channelCopyLink.bind(this));
+      });
 
       // the shift click menu and 3 dots menu on message hover
-      const msgMenuItems = WebpackModules.getByProps("copyLink", "pinMessage");
-      Patcher.instead(msgMenuItems, "copyLink", this.copyLink.bind(this));
+      const msgMenuExport = Webpack.getModule(
+        Filters.byCode(/\)\(\w\.guild_id,\w\.id,\w\.id/),
+        { searchExports: true }
+      );
+      const msgMenuItems = Webpack.getModule((x) =>
+        Object.values(x).includes(msgMenuExport)
+      );
+      const [msgMenuExportName] = Object.entries(msgMenuItems).find(
+        (entry) => entry[1] === msgMenuExport
+      );
+      Patcher.instead(msgMenuItems, msgMenuExportName, this.buttonCopyLink.bind(this));
     }
-    linkItemReplacement(_that, args, reactElement) {
+    onStop() {
+      Patcher.unpatchAll();
+    }
+
+    // modify things that make react elements
+    messageCopyLink(_thisobj, args, reactElement) {
       // `useMessageCopyLinkItem` returns undefined if its not a `SUPPORTS_COPY`
       if (reactElement) {
         // original action:
         // return (0, o.copy)(location.protocol + "//" + location.host + u.Routes.CHANNEL(t.guild_id, t.id, e.id))
         reactElement.props.action = () => {
-          this.copyMessage(args[1], args[0]);
+          this.copyLink(args[1], args[0]);
         };
       }
+      return reactElement;
     }
-    copyLink(_that, args) {
-      this.copyMessage(args[0], args[1]);
+    channelCopyLink(_thisobj, args, reactElement) {
+      // skipping original tracking metadata thing
+      reactElement.props.action = () => {
+        this.copyLink(args[0]);
+      };
+      return reactElement;
     }
-    copyMessage(channel, message) {
-      this.ClipboardUtils.copy(
-        `https://discord.com/channels/${channel.guild_id}/${channel.id}/${message.id}`
-      );
+
+    // function that is stored in the shift button menu giant list of functions
+    buttonCopyLink(_thisobj, args) {
+      this.copyLink(args[0], args[1]);
     }
-    onStop() {
-      Patcher.unpatchAll();
+
+    // utility
+    copyLink(channel, message) {
+      let url;
+      if (message === undefined || message["id"] === undefined) {
+        url =
+          location.protocol +
+          "//" +
+          this.Domain +
+          this.Routes.CHANNEL(channel.guild_id, channel.id);
+      } else {
+        url =
+          location.protocol +
+          "//" +
+          this.Domain +
+          this.Routes.CHANNEL(channel.guild_id, channel.id, message.id);
+      }
+      this.copy(url);
     }
   };
 };
-        return plugin(Plugin, Api);
-    })(global.ZeresPluginLibrary.buildPlugin(config));
-})();
-
-
+     return plugin(Plugin, Api);
+})(global.ZeresPluginLibrary.buildPlugin(config));
 /*@end@*/
--- plugins/NoSpotifyPause.plugin.js
+++ plugins/NoSpotifyPause.plugin.js
@@ -1,72 +1,105 @@
 /**
  * @name NoSpotifyPause
- * @version 0.0.1
+ * @description Prevents Discord from pausing your Spotify when streaming or gaming.
+ * @version 0.0.3
+ * @author bep
+ * @authorId 147077474222604288
  * @authorLink https://github.com/bepvte
+ * @website https://github.com/bepvte/bd-addons
  * @source https://raw.githubusercontent.com/bepvte/bd-addons/main/plugins/NoSpotifyPause.plugin.js
  */
 /*@cc_on
 @if (@_jscript)
-	
-	// Offer to self-install for clueless users that try to run this directly.
-	var shell = WScript.CreateObject("WScript.Shell");
-	var fs = new ActiveXObject("Scripting.FileSystemObject");
-	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
-	var pathSelf = WScript.ScriptFullName;
-	// Put the user at ease by addressing them in the first person
-	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-		shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-	} else if (!fs.FolderExists(pathPlugins)) {
-		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-		// Show the user where to put plugins in the future
-		shell.Exec("explorer " + pathPlugins);
-		shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
-	}
-	WScript.Quit();
+    
+    // Offer to self-install for clueless users that try to run this directly.
+    var shell = WScript.CreateObject("WScript.Shell");
+    var fs = new ActiveXObject("Scripting.FileSystemObject");
+    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
+    var pathSelf = WScript.ScriptFullName;
+    // Put the user at ease by addressing them in the first person
+    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+    } else if (!fs.FolderExists(pathPlugins)) {
+        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+        // Show the user where to put plugins in the future
+        shell.Exec("explorer " + pathPlugins);
+        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+    }
+    WScript.Quit();
 
 @else@*/
-
-module.exports = (() => {
-    const config = {"main":"index.js","info":{"name":"NoSpotifyPause","authors":[{"name":"bep","discord_id":"147077474222604288","github_username":"bepvte"}],"authorLink":"https://github.com/bepvte","version":"0.0.1","description":"Prevents Discord from pausing your Spotify when streaming or gaming.","github":"https://github.com/bepvte/bd-addons","github_raw":"https://raw.githubusercontent.com/bepvte/bd-addons/main/plugins/NoSpotifyPause.plugin.js"}};
-
-    return !global.ZeresPluginLibrary ? class {
-        constructor() {this._config = config;}
-        getName() {return config.info.name;}
-        getAuthor() {return config.info.authors.map(a => a.name).join(", ");}
-        getDescription() {return config.info.description;}
-        getVersion() {return config.info.version;}
-        load() {
-            BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
-                confirmText: "Download Now",
-                cancelText: "Cancel",
-                onConfirm: () => {
-                    require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
-                        if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
-                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
+const config = {
+    main: "index.js",
+    name: "NoSpotifyPause",
+    author: "bep",
+    authorId: "147077474222604288",
+    authorLink: "https://github.com/bepvte",
+    version: "0.0.3",
+    description: "Prevents Discord from pausing your Spotify when streaming or gaming.",
+    github: "https://github.com/bepvte/bd-addons",
+    github_raw: "https://raw.githubusercontent.com/bepvte/bd-addons/main/plugins/NoSpotifyPause.plugin.js",
+    changelog: [
+        {
+            title: "Fixes",
+            type: "fixed",
+            items: [
+                "Fixes the plugin for revamped BetterDiscord! You will need the latest 0PluginLibrary for it to work",
+                "Thank you to devilbro for finding the spotify export! I found it through looking at his plugins",
+                "Extra update to cleanup logs"
+            ]
+        }
+    ]
+};
+class Dummy {
+    constructor() {this._config = config;}
+    start() {}
+    stop() {}
+}
+ 
+if (!global.ZeresPluginLibrary) {
+    BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.name ?? config.info.name} is missing. Please click Download Now to install it.`, {
+        confirmText: "Download Now",
+        cancelText: "Cancel",
+        onConfirm: () => {
+            require("request").get("https://betterdiscord.app/gh-redirect?id=9", async (err, resp, body) => {
+                if (err) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+                if (resp.statusCode === 302) {
+                    require("request").get(resp.headers.location, async (error, response, content) => {
+                        if (error) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), content, r));
                     });
+                }
+                else {
+                    await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
                 }
             });
         }
-        start() {}
-        stop() {}
-    } : (([Plugin, Api]) => {
-        const plugin = (Plugin, Library) => {
-  const { Patcher, WebpackModules } = Library;
+    });
+}
+ 
+module.exports = !global.ZeresPluginLibrary ? Dummy : (([Plugin, Api]) => {
+     const plugin = (Plugin, Library) => {
+  const Filters = Library.Filters;
+  const { Patcher, Webpack } = BdApi;
   return class NoSpotifyPause extends Plugin {
     onStart() {
-      const spotify = WebpackModules.getByProps("fetchIsSpotifyProtocolRegistered");
-      Patcher.instead(spotify, "pause", function(){})
+      const target = Webpack.getModule(Filters.byCode(/SPOTIFY_PLAYER_PAUSE/), {
+        searchExports: true,
+      });
+      const spotifyModule = Webpack.getModule((x) => Object.values(x).includes(target));
+      const [spotifyExportName] = Object.entries(spotifyModule).find(
+        (entry) => entry[1] === target
+      );
+      Patcher.instead("NoSpotifyPause", spotifyModule, spotifyExportName, function () {});
     }
     onStop() {
-      Patcher.unpatchAll();
+      Patcher.unpatchAll("NoSpotifyPause");
     }
   };
 };
-        return plugin(Plugin, Api);
-    })(global.ZeresPluginLibrary.buildPlugin(config));
-})();
-
-
+     return plugin(Plugin, Api);
+})(global.ZeresPluginLibrary.buildPlugin(config));
 /*@end@*/
